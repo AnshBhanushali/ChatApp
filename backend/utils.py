a7660fe2831727afe4217501bb7671618c24cedb
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-08-08 20:46:34 -0400

    created translator

--- backend/utils.py
+++ backend/utils.py
@@ -4,9 +4,14 @@ from googletrans import Translator
 import openai
 import os
 
-# Load NLP models
+# load NLP models
 nlp = spacy.load("en_core_web_sm")
 sentiment_analyzer = pipeline("sentiment-analysis")
 translator = Translator()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# this functionality will translate all any language
+def translate_message(message, target_language='en'):
+    translated = translator.translate(message, dest=target_language)
+    return translated.text
