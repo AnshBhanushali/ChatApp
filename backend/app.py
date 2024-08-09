5a1342b916083c2678739df6565f3224d6a8f94a
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-08-08 20:05:40 -0400

    setting up backend

--- backend/app.py
+++ backend/app.py
@@ -5,3 +5,14 @@ from flask_socketio import SocketIO
 from pymongo import MongoClient
 from config import Config
 import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# JWT Setup
+jwt = JWTManager(app)
+
+# CORS Setup
+CORS(app)
