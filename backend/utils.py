caa7a46b11fa044054065a4b5432fc43fa2b0b0c
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-08-08 20:41:28 -0400

    loading nlp model

--- backend/utils.py
+++ backend/utils.py
@@ -4,3 +4,9 @@ from googletrans import Translator
 import openai
 import os
 
+# Load NLP models
+nlp = spacy.load("en_core_web_sm")
+sentiment_analyzer = pipeline("sentiment-analysis")
+translator = Translator()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
